6c06f7b9f5d33a662aeeb0875b05c64079d9d20a
GuoJiaqi <guojiaqi5946@Outlook.com>
2024-08-27 01:32:02 +0800

    code init

---
+++ src/main/java/com/yupi/springbootinit/mapper/ChartMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.springbootinit.mapper;
+
+import com.yupi.springbootinit.model.entity.Chart;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author HX
+* @description 针对表【chart(帖子)】的数据库操作Mapper
+* @createDate 2024-08-26 22:58:00
+* @Entity com.yupi.springbootinit.model.entity.Chart
+*/
+public interface ChartMapper extends BaseMapper<Chart> {
+
+}
+
+
+
+
---
+++ src/main/java/com/yupi/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.springbootinit.mapper;
+
+import com.yupi.springbootinit.model.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author HX
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2024-08-26 22:58:00
+* @Entity com.yupi.springbootinit.model.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
